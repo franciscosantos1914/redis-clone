eac3bb8b88b0bd62fd3fa49fa5cc5565b8ebc1dc
francisco.santos <>
2024-04-24 11:16:15 +0000

    chore: update jest configs

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,12 @@
 
 module.exports = {
     fakeTimers: {
-      enableGlobally: true,
-      legacyFakeTimers: true,
+      enableGlobally: true
     },
+    clearMocks: true,
+    cache: true,
     testEnvironment: 'node',
     testMatch: ["**.test.mjs"],
-    transformIgnorePatterns: ["/node_modules/", "/build/"]
+    transformIgnorePatterns: ["/node_modules/", "/build/"],
+    coveragePathIgnorePatterns: ["/node_modules/", "/build/"]
   };
