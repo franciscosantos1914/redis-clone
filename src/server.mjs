44754a12bb441fdeafdae9ff0776d41a6ab669c0
francisco.santos <>
2024-04-25 14:19:44 +0000

    chore: capture unknown errors

--- src/server.mjs
+++ src/server.mjs
@@ -1,13 +1,20 @@
+import { env } from 'node:process'
 import { createServer } from 'node:net'
+import { randomUUID } from 'node:crypto'
+
+function attrId(socket) {
+    Reflect.defineProperty(socket, "id", { value: randomUUID() })
+}
 
 createServer()
     .on("connection", socket => {
-        console.log("Client connected: ", socket.address())
+        attrId(socket)
+        console.log("Client connected: ", socket["id"])
         socket
-            .on("data", (data) => console.log(data.toString()))
-            .on("close", () => console.log("Client closed!"))
-            .write("Client connected to the server")
+            .on("data", (data) => console.log(`Client ${socket["id"]} sent: ${data.toString().trim()}`))
+            .on("close", () => console.log(`Client ${socket["id"]} closed!`))
+            .write(`Client connected to the server with id: ${socket["id"]}`)
     })
     .on("close", () => console.log("Server has closed!"))
     .on("error", (err) => console.error(`Unable to run server because of: ${err}`))
-    .listen(3066, () => console.log("Server is up and running on port: 3066"))
+    .listen(env.PORT, () => console.log(`Server is up and running on port: ${env.PORT}`))
